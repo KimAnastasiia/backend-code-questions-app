634c08b38ed32d42048bd202652461a919543ea7
Anastasiia <anstasiakim2789@gmail.com>
2023-08-16 13:53:18 +0200

    edited /questionDB GET for get test just by his id

--- routerQuestionFromDB.js
+++ routerQuestionFromDB.js
@@ -2,12 +2,11 @@ const express = require('express');
 const routerQuestionFromDB = express.Router();
 const database = require("./database")
 
-routerQuestionFromDB.get("/:email/:nameOfTest", async (req, res) => {
-    let email = req.params.email
-    let nameOfTest = req.params.nameOfTest
+routerQuestionFromDB.get("/:id", async (req, res) => {
+    let id = req.params.id
     database.connect();
     try {
-        const results = await database.query("SELECT * FROM createdtests WHERE nameOfTest = ? AND email = ?", [nameOfTest, email])
+        const results = await database.query("SELECT * FROM createdtests WHERE testId = ?", [id])
         database.disConnect()
         return res.send(results)
     } catch (error) {
