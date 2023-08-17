c1bea5ea20e8d350b8f0414b85a375e6af551323
Anastasiia <anstasiakim2789@gmail.com>
2023-08-17 13:32:37 +0200

    edited /question/:email/:name/:id for sent list of questions from db to user

--- routerQuestion.js
+++ routerQuestion.js
@@ -133,59 +133,20 @@ routerQuestion.post("/:email/:name/:id/",async (req,res)=>{
 })
 
 
-routerQuestion.get("/:email/:name/:id", (req,res)=>{
+routerQuestion.get("/:email/:name/:id", async(req,res)=>{
 
     let email = req.params.email
     let name = req.params.name
     let id = req.params.id
-    let allQuestions =[]
-    const allText = fs.readFileSync(`./exams/allTests/${email}${name}${id}.txt`, 'utf-8');
-    let regex = /<<<CODE>>>([\s\S]+?)<<<CODE>>>/g;
-    let code = Array.from(allText.matchAll(regex))
-
-for(let i=0;i<code.length;i++ ){
-
-    let objectQuestion ={}
-
-    regex = /<<<CODE>>>([\s\S]+?)<<<CODE>>>/g;
-    code = Array.from(allText.matchAll(regex))
-
-    objectQuestion.code = code[i][1];
-
-    regex = /-Pregunta([\s\S]+?)<<<CODE>>>/g;
-    let questionText =  Array.from(allText.matchAll(regex))
-
-    objectQuestion.questionText = questionText[i][1];
-
-    regex = /^A\..+/gm;
-    //match = allText.match(regex);
-    let optionA = Array.from(allText.matchAll(regex))
-
-    objectQuestion.optionA = optionA[i][0];
-
-    regex = /^B\..+/gm;
-    //match = allText.match(regex);
-    let optionB = Array.from(allText.matchAll(regex))
-
-    objectQuestion.optionB = optionB[i][0];
-
-    regex = /^C\..+/gm;
-    //match = allText.match(regex);
-    let optionC =Array.from(allText.matchAll(regex))
-
-    objectQuestion.optionC = optionC[i][0];
-
-    regex = /^D\..+/gm;
-    //match = allText.match(regex);
-    let optionD = Array.from(allText.matchAll(regex))
-
-    objectQuestion.optionD = optionD[i][0];
-
-    allQuestions.push(objectQuestion)
-}
-
-
-    res.send(allQuestions)
+    database.connect();
+    try {
+        const results = await database.query("SELECT email, question, answer1, answer2, answer3, answer4, code, nameOfTest  FROM createdtests WHERE testId = ?", [id])
+        database.disConnect()
+        return res.send(results)
+    } catch (error) {
+        database.disConnect()
+        return res.send({ error: error });
+    }
 })
 
 module.exports=routerQuestion
